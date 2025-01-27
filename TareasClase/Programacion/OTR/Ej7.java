1f700b8992285b36e15afa51f32110426d282c59
Eduard Elenchevhttps://github.com/account <44684582+Ardon2222@users.noreply.github.com>
2025-01-27 09:35:59 +0100

    Update Ej7.java

--- TareasClase/Programacion/OTR/Ej7.java
+++ TareasClase/Programacion/OTR/Ej7.java
@@ -3,9 +3,6 @@ import java.util.*;
 public class Ej7 {
     public static void main(String[] args) {
         
-        sdfsdhgfbfvgwdbxiuewgeywdiufbu
-        erjhfgewfheywneifwbgsdk
-        
         Scanner entrada = new Scanner(System.in);
         Random rand = new Random();
 
